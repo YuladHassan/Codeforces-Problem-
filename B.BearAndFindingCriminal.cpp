7177555c8cdf2e7fe7729c8262583a1a64c50689
Yulad Hassan <yuladhassan711@gmail.com>
2022-11-09 19:23:54 +0600

    "first commit"

---
+++ B.BearAndFindingCriminal.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m;
+    cin>>n>>m;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int ans=0;
+    for(int i=0;i<n;i++){
+        int cur=1;
+        for()
+
+    }
+    return 0;
+}
