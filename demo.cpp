eef3306df27f0a82d3fa87c30f7d6b03ae16b6f6
Yulad Hassan <yuladhassan711@gmail.com>
2022-11-12 17:06:16 +0600

    first commit

---
+++ demo.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a[5][5];
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+            cin>>a[i][j];
+        }
+    }
+    //rfoiahf
+    cout<<endl;
+
+    for(int i=0;i<5;i++){
+        cout<<endl;
+        for(int j=0;j<5;j++){
+            cout<<a[i][j]<<" ";
+        }
+    }
+}
